d0f400dd9b6c3eefdb6aa8a10e01824d390ac494
IsadoraFerrao <isadora-gf@hotmail.com>
2022-03-13 16:11:37 -0300

    logic-conditional

---
+++ operador-condicional.py
@@ -0,0 +1,22 @@
+#num1 = int(input('numero 01: '))
+#num2 = int(input('numero 02: '))
+#if num1 > num2:
+#    print('O número maior é: ',num1)
+#else:
+#    print('O número maior é: ',num2)
+    
+n=8
+
+#if n==4:
+#    print('n é igual a 4')
+#else:
+#    if n==3:
+#        print('n é igual a 3')
+#    else:
+#        print('n não é igual a 3 e nem 4')
+x=2
+y=3        
+if(x>1) and (y%2 ==0):
+    print('x é maior que 1 e par')
+else:
+    print('Uma das condições ou as duas não foram satisfeitas')
--- operador-logico.py
+++ operador-logico.py
@@ -4,3 +4,7 @@ b = False
 c= a and b #ambos precisam ser true
 d= a or b #se uma for positiva, ja retorna true
 print(c,'\n',d)
+print(not a) #fazer o contrario
+print(5>3)
+
+
