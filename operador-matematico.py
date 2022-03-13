9dca675b0f34ab9f1e48d2063e554b4db10eb980
IsadoraFerrao <isadora-gf@hotmail.com>
2022-03-13 09:35:10 -0300

    soma

--- operador-matematico.py
+++ operador-matematico.py
@@ -1,10 +1,18 @@
+import math
 a = 5
 b = 3
-print(a+b)
-print(a-b)
-print(a/b)
-print(a*b)
-print('O resto da divisão é', 10 % 3) # % é o simbolo para resto da divisao
+#print(a+b)
+#print(a-b)
+#print(a/b)
+#print(a*b)
+#print('O resto da divisão é', 10 % 3) # % é o simbolo para resto da divisao
 
 a=5**3 #potencia
-print(a)
+#print(a)
+
+print(math.sqrt(a)) #raiz de 25 é 5
+print(round(math.sqrt(a),2)) #raiz de 25 é 5
+
+a= 593.9395939
+print(round(a,3))
+
---
+++ soma.py
@@ -0,0 +1,7 @@
+a = int(input('Digite o primeiro numero: '))
+b = int(input('Digite o segundo numero: '))
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
