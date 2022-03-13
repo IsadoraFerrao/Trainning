a61e5203830a61e4c84a008158eaf7584a902149
IsadoraFerrao <isadora-gf@hotmail.com>
2022-03-13 19:51:00 -0300

    while

---
+++ for.py
@@ -0,0 +1,28 @@
+#print(1)
+#print(2)
+#print(3)
+#print(4)
+#print(5)
+
+#for numero in range(1,6):
+    #print(numero)
+#for numero in range(5,0,-1):
+#    print(numero)
+    
+soma = 0
+#for numero in range(1,6):
+#    soma = soma + numero
+#    print(soma)
+#print(soma)
+
+#palavra = 'sorvete'
+#for letra in palavra:
+    #print(letra)
+    #if(letra == 'v'):
+        #print('Achou o V')
+
+for i in range(1,6):
+    print(i)
+    print('--')
+    for j in range(0,2):
+        print(j)
---
+++ while.py
@@ -0,0 +1,5 @@
+numero = 1
+while numero < 6:
+    print(numero)
+    numero = numero+1
+    numero+=1
