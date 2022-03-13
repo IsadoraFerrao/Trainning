b16490622ea4a0279064facf7493ca34c15276ca
IsadoraFerrao <isadora-gf@hotmail.com>
2022-03-13 09:42:32 -0300

    litros

---
+++ combustivel.py
@@ -0,0 +1,6 @@
+tempo_gasto = int(input('tempo: '))
+velocidade_media = int(input('velocidade: '))
+distancia = tempo_gasto*velocidade_media
+litros = distancia/12
+
+print('Velocidade media', velocidade_media, 'Tempo', tempo_gasto, 'distancia', distancia, 'litros', litros)
