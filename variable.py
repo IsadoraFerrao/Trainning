02c4415aa780551c04fc20de91da28b72ae6cfdc
IsadoraFerrao <isadora-gf@hotmail.com>
2022-03-13 08:54:30 -0300

    dream

--- variable.py
+++ variable.py
@@ -1,10 +1,16 @@
+# int
 numero=-3
 numero_jogos=14
 numero_convidados=15
-
 print(numero, numero_convidados)
 
+#float 
 pi = 3.14
 numero_euler = 2.71
-
 print(pi)
+
+#string
+letra = 'a'
+palavra1 = 'linguagem de programacao'
+palavra2 = 'python'
+print('Estou treinando', letra,palavra1,palavra2)
