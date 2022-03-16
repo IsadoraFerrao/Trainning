2806a01adc3c87947a3020180177a5c9e675eb50
IsadoraFerrao <isadora-gf@hotmail.com>
2022-03-16 01:42:28 -0300

    libraries

---
+++ bibliotecas.py
@@ -0,0 +1,22 @@
+#math
+import math
+print(math.sqrt(36))
+math.sin(45) #seno
+math.cos(45) #cosseno
+m = math.log(1000,10) #logaritimo
+print(m)
+
+#datetime
+import datetime 
+d= datetime.date.today() #saber a data de hoje
+print(d)
+
+h = datetime.datetime.now() #saber o horario exato de agora
+
+date = datetime.date(2020,7,10)
+date.month #para extrair o ano
+
+print(h)
+#random
+
+#time
